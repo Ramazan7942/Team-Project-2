d8a469c53daf6f8b3aa8d0b772ae5b1c89369dae
Ramazan Alipov <ram.alipov@yandex.ru>
2023-03-06 00:34:36 +0300

    Initial commit

--- src/main/java/ru/netology/GameStore.java
+++ src/main/java/ru/netology/GameStore.java
@@ -6,14 +6,31 @@ import java.util.List;
 import java.util.Map;
 
 public class GameStore {
-    private List<Game> games = new ArrayList<>();
 
+    private List<Game> games = new ArrayList<>();
     /**
      * Информация о том, какой игрок сколько играл в игры этого каталога
      * Ключ - имя игрока
      * Значение - суммарное количество часов в игры этого каталога
      */
     private Map<String, Integer> playedTime = new HashMap<>();
+
+
+    public List<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(List<Game> games) {
+        this.games = games;
+    }
+
+    public Map<String, Integer> getPlayedTime() {
+        return playedTime;
+    }
+
+    public void setPlayedTime(Map<String, Integer> playedTime) {
+        this.playedTime = playedTime;
+    }
 
     /**
      * Создание объекта игры с заданными заголовком и жанром
@@ -24,6 +41,8 @@ public class GameStore {
         games.add(game);
         return game;
     }
+
+
 
     /**
      * Проверяет наличие игры в каталоге и возврашает true
