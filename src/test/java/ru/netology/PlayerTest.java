79cc55a9ca2de562fea270626058df8e8a4a1f64
Olia Brizgina <olvitbriz@gmail.com>
2023-03-09 21:38:44 +0400

    6 tests

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,9 +1,33 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class PlayerTest {
+
+    @Test
+    public void shouldInstallOneGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Player player = new Player("Petya");
+        player.installGame(game);
+        assertTrue(store.containsGame(game));
+    }
+    @Test
+    public void shouldInstallTwoGame() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Баттл Онлайн-2", "Аркады");
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        assertTrue(store.containsGame(game1)&& store.containsGame(game2));
+    }
+
 
     @Test
     public void shouldSumGenreIfOneGame() {
@@ -16,9 +40,10 @@ public class PlayerTest {
 
         int expected = 3;
         int actual = player.sumGenre(game.getGenre());
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
-   @Test
+
+    @Test
     public void shouldSumGenreIfTwoGame() {
         GameStore store = new GameStore();
         Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
@@ -29,10 +54,9 @@ public class PlayerTest {
         player.play(game1, 3);
         player.play(game2, 5);
         int expected = 8;
-        int actual = player.sumGenre(game1.getGenre())+player.sumGenre(game2.getGenre());
-        assertEquals(expected, actual);
+        int actual = player.sumGenre(game1.getGenre()) + player.sumGenre(game2.getGenre());
+        Assertions.assertEquals(expected, actual);
     }
-
 
 
     @Test
@@ -52,8 +76,9 @@ public class PlayerTest {
         player.play(game3, 8);
         player.play(game4, 1);
 
-        assertEquals(game3, player.mostPlayerByGenre("Аркады"));
+        Assertions.assertEquals(game3, player.mostPlayerByGenre("Аркады"));
     }
+
     @Test
     public void shouldFindMostPlayerByNotExistGenreGame() {
         GameStore store = new GameStore();
@@ -71,7 +96,7 @@ public class PlayerTest {
         player.play(game3, 8);
         player.play(game4, 1);
 
-        assertEquals(null, player.mostPlayerByGenre("Бродилка"));
+        Assertions.assertEquals(null, player.mostPlayerByGenre("Бродилка"));
     }
 
 
